d1c4e3c34f4a1875a3fc0f5e36cb12aca56ee9e9
Sabrina Moreira <sabrinaarando2002@gmail.com>
2026-06-12 20:19:57 -0300

    fix: cors para produccion

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://localhost:3000",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
